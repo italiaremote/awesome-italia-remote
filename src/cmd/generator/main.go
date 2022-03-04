ee00fe36a8d7d29a879eee5ff5a92b3ed091e4e5
Alessandro Marino <alessandromarinoac@gmail.com>
2022-03-04 21:59:26 +0100

    Update

--- src/cmd/generator/main.go
+++ src/cmd/generator/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/italiaremote/awesome-italia-remote/pkg/cmp"
 	"log"
 	"os"
 	"text/template"
+
+	"github.com/italiaremote/awesome-italia-remote/pkg/cmp"
 )
 
 const homePath = "../../../"
@@ -13,37 +14,32 @@ const dataPath = homePath + "data/"
 
 func main() {
 	companies := make(map[string][]cmp.Company)
-	sections, err := os.ReadDir(dataPath)
+	companiesFile, err := os.ReadDir(dataPath)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	for _, sectionFile := range sections {
-		section := sectionFile.Name()
-		companyFiles, err := os.ReadDir(dataPath + section)
+
+	for _, companyFiles := range companiesFile {
+		var company cmp.Company
+		file, err := os.ReadFile(dataPath + companyFiles.Name())
 		if err != nil {
+			log.Println(companyFiles.Name())
 			log.Fatalln(err)
 		}
 
-		for _, companyFiles := range companyFiles {
-			var company cmp.Company
-			file, err := os.ReadFile(dataPath + section + "/" + companyFiles.Name())
-			if err != nil {
-				log.Println(companyFiles.Name())
-				log.Fatalln(err)
-			}
+		err = json.Unmarshal(file, &company)
+		if err != nil {
+			log.Println(companyFiles.Name())
+			log.Fatalln(err)
+		}
+		err = company.Validate()
+		if err != nil {
+			log.Println(companyFiles.Name())
+			log.Fatalln(err)
+		}
 
-			err = json.Unmarshal(file, &company)
-			if err != nil {
-				log.Println(companyFiles.Name())
-				log.Fatalln(err)
-			}
-			err = company.Validate()
-			if err != nil {
-				log.Println(companyFiles.Name())
-				log.Fatalln(err)
-			}
-
-			companies[section] = append(companies[section], company)
+		for _, category := range company.Categories {
+			companies[category] = append(companies[category], company)
 		}
 	}
 
