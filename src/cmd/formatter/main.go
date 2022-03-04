235e8dac31f20ca8582641cc3179a6d4802f9e5c
Alessandro Marino <alessandromarinoac@gmail.com>
2022-03-04 22:08:19 +0100

    Update formatter

--- src/cmd/formatter/main.go
+++ src/cmd/formatter/main.go
@@ -2,48 +2,42 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/italiaremote/awesome-italia-remote/pkg/cmp"
 	"log"
 	"os"
+
+	"github.com/italiaremote/awesome-italia-remote/pkg/cmp"
 )
 
 const homePath = "../../../"
 const dataPath = homePath + "data/"
 
 func main() {
-	sections, err := os.ReadDir(dataPath)
+	companiesFile, err := os.ReadDir(dataPath)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	for _, sectionFile := range sections {
-		section := sectionFile.Name()
-		companyFiles, err := os.ReadDir(dataPath + section)
+
+	for _, companyFiles := range companiesFile {
+		fileName := dataPath + companyFiles.Name()
+		file, err := os.ReadFile(fileName)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		var company cmp.Company
+
+		err = json.Unmarshal(file, &company)
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		for _, companyFiles := range companyFiles {
-			fileName := dataPath + section + "/" + companyFiles.Name()
-			file, err := os.ReadFile(fileName)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			var company cmp.Company
+		jsonByte, err := json.MarshalIndent(company, "", "  ")
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-			err = json.Unmarshal(file, &company)
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			jsonByte, err := json.MarshalIndent(company, "", "  ")
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			err = os.WriteFile(fileName, jsonByte, 0644)
-			if err != nil {
-				log.Fatalln(err)
-			}
+		err = os.WriteFile(fileName, jsonByte, 0644)
+		if err != nil {
+			log.Fatalln(err)
 		}
 	}
 }
